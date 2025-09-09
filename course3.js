e35616077aef82a8c19b63bb264cc16b22ae4f55
Rahul Dabola <rahuldabola32@gmail.com>
2025-09-10 04:55:51 +0530

    today commit

--- course3.js
+++ course3.js
@@ -15,4 +15,6 @@ const life_name='games';
 function newfeature(){
     console.log('this is a new feature')
     console.log('one more new feature')
+    console.log('random feature1')
+    console.log('random features2')
 }
